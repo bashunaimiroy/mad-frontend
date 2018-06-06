d9755f51f4cc3e1d4f3fa938b724a0ea691e73cd
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2018-06-06 16:51:50 -0400

    [KAT-1] add french about us, some styling

--- src/components/About.js
+++ src/components/About.js
@@ -4,16 +4,26 @@ import {Link} from 'react-router-dom'
 
 
 class About extends Component {
+  constructor(){
+    super()
+    this.state = {
+      language: "en"
+    }
+  }
+  changeLanguage = (langCode) => {
+    this.setState( {language:langCode} )
+  }
   render() {
-    return (
-      <div className="about">
-        {/* <h1 className="about__heading">
-          KickDrum | Katalogue
-        </h1> */}
+    return this.state.language === "en" ? 
+      <div className="about">       
+        <div className="about__section about__section--center">
+          <span className="about__language-link" onClick={()=>this.changeLanguage("en")} >en</span> / <span className="about__language-link" onClick={()=>this.changeLanguage("fr")} >fr</span>
+        </div>
 
-        <div className="about__section"><h2 className="about__subheading">
-          Our Mission
-        </h2>
+        <div className="about__section">
+          <h2 className="about__subheading">
+            Our Mission
+          </h2>
 
           <p className="about__paragraph">
             To make <em>listening local</em> simple and compelling
@@ -113,7 +123,7 @@ class About extends Component {
 
           <p className="about__paragraph">
             Follow us on our social media accounts below
-</p>
+          </p>
 
           <p className="social-media-url"><a href="https://www.facebook.com/kickdrumMTL">Facebook</a></p>
           <p className="social-media-url"><a href="https://instagram.com/kickdrummtl/">Instagram</a></p>
@@ -125,7 +135,137 @@ class About extends Component {
         </div>
 
       </div>
-    );
+
+      :
+
+      <div className="about">       
+        <div className="about__section about__section--center">
+          <span className="about__language-link" onClick={()=>this.changeLanguage("fr")} >fr</span> / <span className="about__language-link" onClick={()=>this.changeLanguage("en")} >en</span>
+        </div>
+
+        <div className="about__section">
+          <h2 className="about__subheading">
+          Notre mission
+          </h2>
+
+          <p className="about__paragraph">
+          Qu’<em>écouter localement</em> devienne simple et passionnant.  
+        </p></div>
+
+        <div className="about__section"><h2 className="about__subheading">
+        Notre message
+        </h2>
+
+          <p className="about__paragraph">
+          Montréal abrite une scène musicale vibrante et dynamique.  Ceci n’est pas dû aux nombreux festivals et tournées internationaux, mais plutôt aux artistes locaux talentueux et « underground » qui y habitent.   
+        </p>
+
+          <p className="about__paragraph">
+          Les salles indépendantes contribuent énormément à l’atmosphère musicale de Montréal, car elles offrent à nos artistes des opportunités accessibles pour se faire connaitre et se développer.  C’est la musique jouée dans les espaces comme la Casa del Popolo et l’Escogriffe, et non au Centre Bell ou MTelus, qui rend Montréal unique.  
+        </p>
+
+          <p className="about__paragraph">
+          Cependant, dû aux hausses des prix de locations, plusieurs de ses salles indépendantes sont obligés de fermer ou de se déplacer (e.g. Divan Orange et le Cagibi. Les artistes ayant vécus et contribués à la construction la notre capitale culturelle de quartiers comme le Mile End sont obligés de donner place aux propriétés commerciales et marques internationales.  
+        </p>
+
+          <p className="about__paragraph">
+          Dans un environnement comme le notre, célébrer et supporter nos artistes locaux est d’une importance majeure... à moins que vous vouliez vivre dans une ville comme Toronto?
+          </p></div>
+
+        <div className="about__section"><h2 className="about__subheading">
+        Notre inspiration
+        </h2>
+
+          <p className="about__paragraph">
+            <em>Trouver des artistes locaux que vous aimez ne devrait pas être difficile.</em>
+          </p>
+
+          <p className="about__paragraph">
+          Les principales plateformes de diffusion telle que Spotify et Apple Music ne permettent pas aux auditeurs de parcourir les artistes par localisation, et ne permettent que quelques options de playlists associées à certaines villes.  Ceci veut dire que si vous désirez écouter de la musique locale, vous devez chercher indirectement – sur google, les médias de niche, bouche à oreille, ou les listes d’évènements locaux.  Découvrir de la musique créée dans ta propre arrière-cour ne devrait pas être si difficile!  
+        </p>
+
+          <p className="about__paragraph">
+          Donc, nous avons travaillé fort pour vous en compilant une liste croissante de plus de 1,000 artistes et groupes de musique locaux à un endroit!  
+        </p>
+
+          <p className="about__paragraph">
+          Découvrez de la musique que vous aimerez en naviguant, en écoutant nos listes de lecture organisées, en recherchant à travers une grande variété d’étiquettes de genres différentes, ou en consultant nos listes de concerts locaux! 
+
+        </p></div>
+
+
+
+        <div className="about__section"><h2 className="about__subheading">
+        Pourquoi est-ce important d’écouter local
+        </h2>
+
+          <p className="about__paragraph">
+          Les avantages de l’achat local tel que la transparence et l’équité salarial sont reconnus et célébrés.  Que ce soit l’achat de vêtements ou de l’épicerie, les gens se préoccupent de plus en plus e l’origine et le processus de production du produit.  Par contre, cette préoccupation semble absente quand il s’agit de musique, malgré le fait que l’écoute locale implique l’achat local.  Voici quelques bonnes raisons d’écouter de la musique locale: 
+        </p>
+
+          <p className="about__paragraph">
+            <ul className="about__list">
+              <li>Soutenir des artistes locaux signifie que votre argent demeurera dans l’économie locale et créera donc, dans l’ensemble, des opportunités pour la communauté artistique locale: des sites, labels, festivals, producteurs et promoteur d’évènements, agents, et plus encore! </li>
+              <li>Assister à un concert dans un site local à petite échelle est la meilleure façon d’assurer l’existence de notre vibrante scène musicale!
+</li>
+              <li>Vous aurez plus d’opportunités d’assister à une performance d’un artiste local et de vivre des interactions significatives.  </li>
+              <li>Observer le développement d’un artiste au fil du temps est une expérience unique et enrichissante. </li>
+              <li>Voir la performance d’un artiste local est souvent moins couteux que celui d’artiste en tournée. </li>
+              <li>Vous pourriez découvrir avec une nouvelle appréciation pour votre communauté.  </li>
+            </ul>
+          </p></div>
+
+        <div className="about__section"><h2 className="about__subheading">
+        Soumissions
+</h2>
+
+          <p className="about__paragraph">
+          Si vous êtes un artiste ou groupe de musique basé à Montréal et n’êtes pas sur notre base de donnée, dirigez-vous vers notre page de soumission!  Il nous fera plaisir de vous ajouter! 
+        </p>
+
+          <p className="about__paragraph">
+          Aussi, si vous notez des liens brisés ou des informations fausses, manquantes ou périmées, nous apprécierons énormément que vous <a href="mailto:events@kickdrum.ca">nous envoyiez un message!</a>
+        </p></div>
+
+        <div className="about__section"><h2 className="about__subheading">
+        À propos de KickDrum
+        </h2>
+
+          <p className="about__paragraph">
+          KickDrum est un organisme à but non lucrative qui produit et fait la promotion d’évènements musicaux.  C’est un organisme axé sur la formation de relations durables et à soutenir les communautés musicales locales.         
+          </p>
+
+          <p className="about__paragraph">
+          Depuis 2015, nous avons travaillé équitablement avec plus de 250 artistes/groupes et organisé plus de 160 évènements.  
+        </p></div>
+
+        <div className="about__section"><h2 className="about__subheading">
+        Contactez-nous 
+        </h2>
+
+          <p className="about__paragraph">
+          Pour toutes questions svp écrivez-nous à <a href="mailto:events@kickdrum.ca">events@kickdrum.ca</a> ou envoyez-nous un message directement sur <a href="https://www.facebook.com/kickdrumMTL">Facebook</a>!              
+        </p>
+
+          <p className="about__paragraph">
+          Suivez-nous sur nos médias sociaux aux liens suivants: 
+          </p>
+
+          <p className="social-media-url"><a href="https://www.facebook.com/kickdrumMTL">Facebook</a></p>
+          <p className="social-media-url"><a href="https://instagram.com/kickdrummtl/">Instagram</a></p>
+          <p className="social-media-url"><a href="https://twitter.com/KickDrumMtl">Twitter</a></p>
+          <p className="social-media-url"><a href="https://www.youtube.com/channel/UCs_OZ1ylUehBu7zEbiAm-oA">Youtube</a></p>
+          <p className="social-media-url"><a href="http://spoti.fi/2H3XdzC">Spotify</a></p>
+          <p className="social-media-url"><a href="https://soundcloud.com/kickdrummtl">Soundcloud</a></p>
+          <p className="social-media-url"><a href="https://playmoss.com/en/kickdrummtl">Playmoss</a></p>
+        </div>
+
+      </div>
+    
+    
+
+      
+    
   }
 }
 
